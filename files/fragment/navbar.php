2d24776494ff534a8160345000f886ddfb2d65c0
Raihan Alifianto <purplelamborghini24@gmail.com>
2021-11-04 17:30:58 +0700

    push to server

--- files/fragment/navbar.php
+++ files/fragment/navbar.php
@@ -2,10 +2,10 @@
   <img class="logo-brand" src="../img/logo.jpg" style="width: 5%; height: 5%">
   <ul class="nav justify-content-end">
     <li class="nav-item">
-        <a class="nav-link active" href="../pages/table-page.php">Table</a>
+        <a class="nav-link active" href="../table-page.php">Table</a>
     </li>
     <li class="nav-item">
-      <a class="nav-link" href="../pages/chart-page.php">Chart</a>
+      <a class="nav-link" href="../chart-page.php">Chart</a>
     </li>
     <li class="nav-item">
       <a class="nav-link text-danger" href="../php/logout.php">Logout</a>
