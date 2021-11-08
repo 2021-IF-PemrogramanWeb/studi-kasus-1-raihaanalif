d5e529772cb4a85ace51907160cae5a4e806a412
Raihan Alifianto <purplelamborghini24@gmail.com>
2021-11-08 23:54:47 +0700

    push to server

--- files/php/config.php
+++ files/php/config.php
@@ -7,7 +7,8 @@ $port = "5432";
 
 function pg_connection_string_from_database_url() {
     extract(parse_url($_ENV["postgres://ebcvshzketwbsr:7b94dc82d5af9da75816084a84245448b6b74231dba72bb176a1cbe7ed0726a8@ec2-3-218-92-146.compute-1.amazonaws.com:5432/d96l3e2fpq82i7"]));
-    return "user=$user password=$pass host=$host dbname=$dbname port=$port"; # <- you may want to add sslmode=require there too
+    $path = ltrim($path, "/");
+    return "user=$user password=$pass host=$host dbname=$dbname port="; # <- you may want to add sslmode=require there too
   }
   
 $connect = pg_connect(pg_connection_string_from_database_url());
