8ee95eb4981c668086193d986f1a08a7d75ce405
Raihan Alifianto <purplelamborghini24@gmail.com>
2021-11-09 00:33:38 +0700

    push to server

--- files/php/login.php
+++ files/php/login.php
@@ -5,7 +5,7 @@ $pass = $_POST["password"];
 
 if($email == "raihanalifianto@gmail.com" && $pass == "test1234"){
     $_SESSION['email'] = $email;
-    setcookie("email", $email, time() + 30);
+    setcookie('email', $email, time() + 30);
     header('location:../table-page.php?pesan=berhasil');
 }
 else{
