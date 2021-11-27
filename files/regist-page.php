3b6fe73816a29f5966184cc4c112d59df8a342e5
Raihan Alifianto <purplelamborghini24@gmail.com>
2021-11-27 19:35:54 +0700

    fix bug

--- files/regist-page.php
+++ files/regist-page.php
@@ -38,12 +38,6 @@
                         <input type="password" class="form-control" id="inputPassword" name="password">
                     </div>
                 </div>
-                <div class="mb-3 row">
-                    <label for="inputPassword" class="col-sm-2 col-form-label">Password</label>
-                    <div class="col-sm-10">
-                        <input type="password" class="form-control" id="inputPassword" name="password">
-                    </div>
-                </div>
                 <a href="#" style="text-decoration: none; color: black;"><b>Remember Me</b></a>
                 <input type="submit" class="btn btn-primary" style="float: right;">
             </form>
