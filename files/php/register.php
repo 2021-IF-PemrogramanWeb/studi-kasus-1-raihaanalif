d7296b08789db18e24a438754bf9fe417b1d644d
Raihan Alifianto <purplelamborghini24@gmail.com>
2021-11-29 08:15:29 +0700

    fix bug

--- files/php/register.php
+++ files/php/register.php
@@ -9,7 +9,7 @@ $password = $_POST['password'];
 $after_hash = md5($password);
 
 $query = $connect->query("insert into account (nama, email, passwords) values ('$nama', '$email', '$after_hash')");
-if(!query){
+if(!$query){
     header("location:../regis-page.php?pesan=gagal");
 }else{
     header("location:../index.php?pesan=register");
