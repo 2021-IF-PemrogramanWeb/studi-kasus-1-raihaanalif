f7bd98a26ce6f5dcbca1652a216883feb588cb17
Raihan Alifianto <purplelamborghini24@gmail.com>
2021-11-29 07:58:13 +0700

    fix bug

--- files/php/register.php
+++ files/php/register.php
@@ -8,7 +8,7 @@ $password = $_POST['password'];
 
 $after_hash = md5($password);
 
-$query = $connect->query('insert into account ("nama", "email", "passwords") values ("$name", "$email", "$after_hash")');
+$query = $connect->query('insert into account values ("$name", "$email", "$after_hash")');
 if(!query){
     header("location:../regis-page.php?pesan=gagal");
 }else{
